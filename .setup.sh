a29c44a569b357a2a8a1946ba418e174352a822c
Brian C. Milco <bcmilco@gmail.com>
2024-03-23 21:56:58 -0400

    Flush out setup script, make it more robust

--- .setup.sh
+++ .setup.sh
@@ -1,26 +1,55 @@
-#!/bin/which sh
+#!/usr/bin/env bash
 #
 # Installing software for use with the dot files in this repo
 
-# Fuzzy Finder
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+set -eou pipefail
 
-# Oh-my-Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+font_name="FiraCode"
 
-# Powerline 10k
+echo "Install applicatoins"
+sudo apt install -y zsh vim
+sudo usermod -s /usr/bin/zsh $USER
 
-# Donwload a Nerd Font:
-#https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts
-curl
-https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/FiraCodeNerdFontMono-Regular.ttf \
-  -o $HOME/.fonts/FiraCodeNerdFontMono-Regualar.ttf
+if [ ! -d $HOME/.fzf ]
+then 
+  echo "--==[[ Install Fuzzy Finder ]]==--"
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install --bin --no-update-rc
+fi
 
-# Download Powerline
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+if [ ! -d $HOME/.oh-my-zsh ]
+then 
+  echo "--==[[ Install oh-my-zsh ]]==--"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended --keep-zshrc"
 
-# Vim Plugins
-sudo apt install vim -y
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  echo "--==[[ Install oh-my-zsh plugins ]]==--"
+  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+fi 
+
+nerd_font="${font_name}NerdFontMono-Regular.ttf"
+if [ ! -d "$HOME/.fonts/${nerd_font}" ]
+then
+  echo "--==[[ Install nerd fonts ]]==--"
+  mkdir -p $HOME/.fonts
+  #https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts
+  curl \
+    https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/${font_name}/Regular/${nerd_font} \
+    -o $HOME/.fonts/${nerd_font}
+fi
+
+if [ ! -d $HOME/.oh-my-zsh/custom/themes/powerlevel10k ]
+then
+  echo "--==[[ Install Powerlevel 10k ]]==--"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+
+if [ ! -d $HOME/.vim/bundle/Vundle.vim ]
+then
+  echo "--==[[ Install Vundle ]]==--"
+  mkdir -p ~/.vim/bundle
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+echo "--==[[ Install vim plugins ]]==--"
 vim -c PluginInstall -c qall
